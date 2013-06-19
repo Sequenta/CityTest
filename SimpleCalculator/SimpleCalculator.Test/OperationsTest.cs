1e367ba579dd08473440d48c6aa2e2e241b0cb23
Axel <tranquilized.mind@gmail.com>
2013-06-19 19:10:01 +0500

    Тест для операции +

---
+++ SimpleCalculator/SimpleCalculator.Test/OperationsTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace SimpleCalculator.Test
+{
+    public class OperationsTest
+    {
+        Calculator calculator = new Calculator();
+
+        [Fact]
+        public void PlusOperation_ReturnsCorrectValue()
+        {
+            var result = calculator.Add(2, 2);
+            
+            Assert.Equal(4,result);
+        }
+    }
+}
